5e2e8c3721f53396bcadd020f03501639e757cfb
Eliav Opatowsky <eliav.opatowsky.45@gmail.com>
2024-01-17 12:28:44 +0200

    fixed bugs in seq_align.py

--- seq_align.py
+++ seq_align.py
@@ -1,4 +1,4 @@
-# import argparse
+import argparse
 import numpy as np
 from itertools import groupby
 from enum import Enum
@@ -16,37 +16,69 @@ class directions(Enum):
     LAU = 3
 
 
+def print_format(s1, s2, score, align_type='global'):
+    """
+    prints
+    :param s1:
+    :param s2:
+    :param score:
+    :param align_type:
+    :return:
+    """
+    s_blocks1 = [s1[n:n + 50] for n in range(0, len(s1), 50)]
+    s_blocks2 = [s2[n:n + 50] for n in range(0, len(s2), 50)]
+    for i in range(len(s_blocks1)):
+        print(''.join(s_blocks1[i]))
+        print(''.join(s_blocks2[i]))
+        print()
+    print(align_type + ":" + str(score))
+
+
 def overlap_seg(s, t):
     seg_array = np.empty((len(s)+1, len(t)+1), dtype=object)
 
-    seg_array[0,0] = (0, directions.START)
+    seg_array[0,0] = (0, directions.START, 0)
     for i in range(len(s)):
-        seg_array[i+1,0] = (0, directions.UP)
+        seg_array[i+1,0] = (0, directions.UP, 1)
     for i in range(len(t)):
-        seg_array[0,i+1] = ((i+1)*(-2), directions.LEFT)
+        seg_array[0, i+1] = (0, directions.LEFT, 2)
 
     for j in range(len(s)):
         for i in range(len(t)):
-            if j+1!=len(s):
-                left_op = seg_array[j+1, i][0] + score_table[serialize_dict[s[j]], serialize_dict['-']]
-                up_op = seg_array[j, i+1][0] + score_table[serialize_dict['-'], serialize_dict[t[i]]]
-                lau_op = seg_array[j, i][0] + score_table[serialize_dict[s[j]], serialize_dict[t[i]]]
-            else:
+            status = False
+
+            status_left = seg_array[j+1, i][2]
+            status_up = seg_array[j, i+1][2]
+            status_lau = seg_array[j, i][2]
+
+            if j+1==len(s) and status_left != 2:
                 left_op = seg_array[j+1, i][0]
+            else:
+                left_op = seg_array[j+1, i][0] + score_table[serialize_dict[s[j]], serialize_dict['-']]
+
+            if i+1==len(t) and status_up != 1:
                 up_op = seg_array[j, i+1][0]
-                lau_op = seg_array[j, i][0]
+            else:
+                up_op = seg_array[j, i+1][0] + score_table[serialize_dict['-'], serialize_dict[t[i]]]
+
+            lau_op = seg_array[j, i][0] + score_table[serialize_dict[s[j]], serialize_dict[t[i]]]
 
             m = max(left_op, up_op, lau_op)
 
             if m==left_op:
                 direction = directions.LEFT
+                status = status_left
             elif m==up_op:
                 direction = directions.UP
+                status = status_up
             else:
                 direction = directions.LAU
+                status = status_lau
             
+            # print(f"seg_array[{i+1},{j+1}] = {m} and direction is {direction}")
+            seg_array[j+1, i+1] = (m, direction, status)
 
-            seg_array[j+1, i+1] = (m, direction, f"{s[j]},{t[i]}")
+    # print(seg_array)
 
     curr_s_index, curr_t_index = len(s), len(t)
 
@@ -65,26 +97,34 @@ def overlap_seg(s, t):
         else:
             pass
 
-    return ''.join(s), ''.join(t), seg_array[-1, -1][0]
+    return s, t, seg_array[-1, -1][0]
 
 
 def local_seg(s, t):
     seg_array = np.empty((len(s)+1, len(t)+1), dtype=object)
 
+    score_i, score_j = 0, 0
+    score = 0
+
     seg_array[0,0] = (0, directions.START)
     for i in range(len(s)):
-        seg_array[i+1,0] = ((i+1)*(-2), directions.UP)
+        seg_array[i+1,0] = ((i+1)*score_table[serialize_dict[s[i]], serialize_dict['-']], directions.UP)
     for i in range(len(t)):
-        seg_array[0,i+1] = ((i+1)*(-2), directions.LEFT)
+        seg_array[0,i+1] = ((i+1)*score_table[serialize_dict['-'], serialize_dict[t[i]]], directions.LEFT)
 
     for j in range(len(s)):
         for i in range(len(t)):
             left_op = seg_array[j+1, i][0] + score_table[serialize_dict[s[j]], serialize_dict['-']]
             up_op = seg_array[j, i+1][0] + score_table[serialize_dict['-'], serialize_dict[t[i]]]
             lau_op = seg_array[j, i][0] + score_table[serialize_dict[s[j]], serialize_dict[t[i]]]
-            start_op = 0
 
-            m = max(left_op, up_op, lau_op, start_op)
+            start_left_op  = score_table[serialize_dict[s[j]], serialize_dict['-']]
+            start_up_op = score_table[serialize_dict['-'], serialize_dict[t[i]]]
+            start_lau_op = score_table[serialize_dict[s[j]], serialize_dict[t[i]]]
+
+            m = max(left_op, up_op, lau_op, start_left_op, start_up_op, start_lau_op)
+            if m > score:
+                score, score_i, score_j = m, i, j
 
             if m==lau_op:
                 direction = directions.LAU
@@ -97,29 +137,31 @@ def local_seg(s, t):
             
 
             seg_array[j+1, i+1] = (m, direction, f"{s[j]},{t[i]}")
-            # print(f"seg_array[{j+1}, {i+1}]={seg_array[j+1, i+1]}")
 
-    curr_s_index, curr_t_index = len(s), len(t)
+    curr_s_index, curr_t_index = score_j + 1, score_i + 1
+    ret_s, ret_t = [], []
+    direction = directions.LAU
 
-    # print(s, t)
-    while curr_s_index!=0 or curr_t_index!=0:
-        direction = seg_array[curr_s_index,curr_t_index][1]
-        # print(f"s_ind:{curr_s_index}, t_ind:{curr_t_index}, direction:{direction}")
-        if direction==directions.LAU:
-            curr_s_index, curr_t_index = curr_s_index-1, curr_t_index-1
-        elif direction==directions.LEFT:
-            s.insert(curr_s_index, '-')
-            curr_t_index -= 1
-        elif direction==directions.UP:
-            t.insert(curr_t_index, '-')
+    while seg_array[curr_s_index, curr_t_index][1] != directions.START:
+        if direction == directions.LAU:
+            ret_s.insert(0, s[curr_s_index-1])
+            ret_t.insert(0, t[curr_t_index-1])
             curr_s_index -= 1
+            curr_t_index -= 1
+
+        elif direction == directions.LEFT:
+            ret_s.insert(0, '-')
+            ret_t.insert(0, t[curr_t_index-1])
+            curr_t_index -= 1
+
         else:
-            t.insert(curr_t_index, ' ')
-            s.insert(curr_s_index, ' ')
+            ret_s.insert(0, s[curr_s_index-1])
+            ret_t.insert(0, '-')
             curr_s_index -= 1
-            curr_t_index -= 1
 
-    return ''.join(s), ''.join(t), seg_array[-1, -1][0]
+        direction = seg_array[curr_s_index, curr_t_index][1]
+
+    return ret_s, ret_t, score
 
 
 def global_seg(s, t):
@@ -127,9 +169,9 @@ def global_seg(s, t):
 
     seg_array[0,0] = (0, directions.START)
     for i in range(len(s)):
-        seg_array[i+1,0] = ((i+1)*(-2), directions.UP)
+        seg_array[i+1,0] = ((i+1)*score_table[serialize_dict[s[i]], serialize_dict['-']], directions.UP)
     for i in range(len(t)):
-        seg_array[0,i+1] = ((i+1)*(-2), directions.LEFT)
+        seg_array[0,i+1] = ((i+1)*score_table[serialize_dict['-'], serialize_dict[t[i]]], directions.LEFT)
 
     for j in range(len(s)):
         for i in range(len(t)):
@@ -167,7 +209,7 @@ def global_seg(s, t):
         else:
             pass
 
-    return ''.join(s), ''.join(t), seg_array[-1, -1][0]
+    return s, t, seg_array[-1, -1][0]
 
 def fastaread(fasta_name):
     """
@@ -210,18 +252,22 @@ def main():
     
     if command_args.align_type == 'global':
         res = global_seg(s, t)
-        print(f'{res[0]}\n\n{res[1]}\n\nglobal:{res[2]}')
+        print_format(res[0], res[1], res[2], 'global')
     elif command_args.align_type == 'local':
         res = local_seg(s, t)
-        print(f'{res[0]}\n\n{res[1]}\n\nlocal:{res[2]}')
+        print_format(res[0], res[1], res[2], 'local')
     elif command_args.align_type == 'overlap':
         res = overlap_seg(s, t)
-        print(f'{res[0]}\n\n{res[1]}\n\noverlap:{res[2]}')
+        print_format(res[0], res[1], res[2], 'overlap')
     elif command_args.align_type == 'global_lin':
         raise NotImplementedError
     # print the best alignment and score
 
 
 if __name__ == '__main__':
-    # print(global_seg(list("TAAGCTA"), list("AGTA")))
+    # tsv = pd.read_table('score_matrix.tsv')
+    # del tsv[tsv.columns[0]]
+    # score_table = np.array(tsv)
+
+    # print(local_seg(list("TACCG"), list("TAG")))
     main()
